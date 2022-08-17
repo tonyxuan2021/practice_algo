21fb1e93f87b42b7565054f24e5bc1269da63bc7
tonyxuan2021 <tonyyexuan@gmail.com>
2022-08-17 02:16:34 +0000

    Sync LeetCode submission - Binary Tree Preorder Traversal (java)

--- my-folder/problems/binary_tree_preorder_traversal/solution.java
+++ my-folder/problems/binary_tree_preorder_traversal/solution.java
@@ -1,16 +1,19 @@
 // preorder - root, left, right
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> list = new LinkedList<>();
-        if(root == null) return list;
-        builder(root, list);
-        return list;
-    }
-    
-    private void builder(TreeNode root, List<Integer> list){
-        if(root == null) return;
-        list.add(root.val);
-        builder(root.left, list);
-        builder(root.right, list);
+        List<Integer> prelist = new LinkedList<>();
+        if(root == null) return prelist;
+        
+        // stack DS 
+        Deque<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+        
+        while(!stack.isEmpty()){
+            TreeNode curr = stack.pop();
+            prelist.add(curr.val);
+            if(curr.right != null) stack.push(curr.right);
+            if(curr.left != null) stack.push(curr.left);
+        }
+        return prelist;
     }
 }
