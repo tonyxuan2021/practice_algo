8417c34dbdef35dc38e85cb393a8a2854ca24f29
tonyxuan2021 <tonyyexuan@gmail.com>
2022-08-18 03:54:37 +0000

    Sync LeetCode submission - Maximum Depth of Binary Tree (java)

--- my-folder/problems/maximum_depth_of_binary_tree/solution.java
+++ my-folder/problems/maximum_depth_of_binary_tree/solution.java
@@ -1,34 +1,22 @@
-// Max Depth - number of nodes along LONGEST path from root node down to farthest leaf nodes
 class Solution {
-    // Approach 3: Iterative approach
-    // stack DS, one can mimic behaviors of fx call stack involved in recursion 
+    // Top-down solution: pre-order => root, left, right
+    // We go from the root (starting from 1) to the leaves (past it when we hit null that is our base case)
+    private int maxDepth = 0;
+    
     public int maxDepth(TreeNode root) {
-        // two stacks to track nodes and depth
-        Deque<TreeNode> nodeS = new LinkedList<>();
-        Deque<Integer> depthS = new LinkedList<>();
-        
-        int currDepth = 0, maxDepth = 0;
-        
-        nodeS.push(root);
-        depthS.push(1);
-        
         if(root == null) return 0;
-        
-        while(!nodeS.isEmpty()){
-            root = nodeS.pop();
-            currDepth = depthS.pop();
-            
-            if(root != null){
-                nodeS.push(root.left);
-                nodeS.push(root.right);
-                
-                maxDepth = Math.max(maxDepth, currDepth);
-                
-                depthS.push(currDepth + 1);
-                depthS.push(currDepth + 1);
-            }
-        }
+        return maxDepth(root, 1); // pass along 1 as our root value
+    }
+    
+    public int maxDepth(TreeNode node, int depth){
+        if(node == null) return 0;
+        // if we are at a leaf node
+        if(node.left == null && node.right == null) maxDepth = Math.max(maxDepth, depth);
+        // recursively traverse down left and right subtrees and adding 1 to depth at each child level
+        maxDepth(node.left, (depth + 1));
+        maxDepth(node.right, (depth + 1));
         
         return maxDepth;
     }
+    
 }
