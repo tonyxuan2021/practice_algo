1465884f143591e3883d2981f114ada3bd062d32
tonyxuan2021 <tonyyexuan@gmail.com>
2022-08-23 03:50:01 +0000

    Sync LeetCode submission - Maximum Depth of Binary Tree (java)

--- my-folder/problems/maximum_depth_of_binary_tree/solution.java
+++ my-folder/problems/maximum_depth_of_binary_tree/solution.java
@@ -1,22 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// bottoms-up approach
 class Solution {
-    // Top-down solution: pre-order => root, left, right
-    // We go from the root (starting from 1) to the leaves (past it when we hit null that is our base case)
-    private int maxDepth = 0;
-    
     public int maxDepth(TreeNode root) {
-        if(root == null) return 0;
-        return maxDepth(root, 1); // pass along 1 as our root value
-    }
-    
-    public int maxDepth(TreeNode node, int depth){
-        if(node == null) return 0;
-        // if we are at a leaf node
-        if(node.left == null && node.right == null) maxDepth = Math.max(maxDepth, depth);
-        // recursively traverse down left and right subtrees and adding 1 to depth at each child level
-        maxDepth(node.left, (depth + 1));
-        maxDepth(node.right, (depth + 1));
+        int max = 0;
         
-        return maxDepth;
+        if(root == null) return 0;
+        
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        
+        return max = Math.max(leftDepth, rightDepth) + 1;
     }
-    
 }
