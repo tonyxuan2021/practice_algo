2faa11d495106044971b228a547239a7c763f99e
tonyxuan2021 <tonyyexuan@gmail.com>
2022-08-25 03:41:32 +0000

    Sync LeetCode submission - Binary Tree Inorder Traversal (java)

--- my-folder/problems/binary_tree_inorder_traversal/solution.java
+++ my-folder/problems/binary_tree_inorder_traversal/solution.java
@@ -1,23 +1,22 @@
-// inorder -> left, root, right
 class Solution {
+    // iterative solution with stack DS
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> inlist = new LinkedList<>();
-        if(root == null) return inlist;
-            
+        List<Integer> inList = new LinkedList<>();
+        if(root == null) return inList;
+        
         Deque<TreeNode> stack = new LinkedList<>();
+        
         TreeNode curr = root;
         
         while(curr != null || !stack.isEmpty()){
-            // travel to left most node
             while(curr != null){
                 stack.push(curr);
                 curr = curr.left;
             }
-            // pop off our stack and add to our inlist
             curr = stack.pop();
-            inlist.add(curr.val);
+            inList.add(curr.val);
             curr = curr.right;
         }
-        return inlist;
+        return inList;
     }
 }
