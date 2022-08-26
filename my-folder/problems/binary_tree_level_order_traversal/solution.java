cc000645c50ffa4a052eaa3bc98906f1effacc59
tonyxuan2021 <tonyyexuan@gmail.com>
2022-08-26 03:42:02 +0000

    Sync LeetCode submission - Binary Tree Level Order Traversal (java)

--- my-folder/problems/binary_tree_level_order_traversal/solution.java
+++ my-folder/problems/binary_tree_level_order_traversal/solution.java
@@ -1,22 +1,24 @@
-// Given the root of a binary tree, return the level order traversal of its nodes' values 
-// i.e. BFS - root, child left, child right
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
         if(root == null) return result;
         
-        // BFS - queue DS
+        // use Queue interface to perform BFS traversal iteratively
         Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root); 
+        queue.add(root);
         
         while(!queue.isEmpty()){
+            // get the size of our queue for our for-loop operation
             int size = queue.size();
-            List<Integer> currLevel = new LinkedList<>();
+            // create a new ArrayList for each level
+            List<Integer> currLevel = new ArrayList<>();
             
+            // for-loop to add elements in our queue to our currLevel
             for(int i = 0; i < size; i++){
                 TreeNode currNode = queue.poll();
                 currLevel.add(currNode.val);
                 
+                // add currNode's children to our queue for next while-loop iteration
                 if(currNode.left != null) queue.add(currNode.left);
                 if(currNode.right != null) queue.add(currNode.right);
             }
