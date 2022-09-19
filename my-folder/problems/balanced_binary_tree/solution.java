6747dce854cf5b040e551ad120253335988fd9d7
tonyxuan2021 <tonyyexuan@gmail.com>
2022-09-19 16:18:48 +0000

    Sync LeetCode submission - Balanced Binary Tree (java)

--- my-folder/problems/balanced_binary_tree/solution.java
+++ my-folder/problems/balanced_binary_tree/solution.java
@@ -1,39 +1,24 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    final int M = Integer.MIN_VALUE;
+    // variable to keep track of our binary tree is balanced
+    private boolean balancedBool = true;
     
     public boolean isBalanced(TreeNode root) {
-        // use MIN_VALUE because height of null tree is -1 (generally)
-        return checkHeight(root) != M;
+        getHeight(root);
+        return balancedBool;
     }
     
-    // helper fx - recursive
-    public int checkHeight(TreeNode root){
-    	if(root == null) return -1; // null is true and height of null tree is generally -1 
-    
-    	int leftH = checkHeight(root.left);
-    	if(leftH == M) return M;
+    private int getHeight(TreeNode node){
+        // base case
+        if(node == null) return -1;
         
-    	int rightH = checkHeight(root.right);
-    	if(rightH == M) return M;
+        // recursively go down our tree (to the leaf nodes and beyond)
+        int leftSub = getHeight(node.left);
+        int rightSub = getHeight(node.right);
         
-    	if(Math.abs(leftH - rightH) > 1) return M;
-
-        // return height of the overall subtree (+1, current leaf node is technically height 0 because null node has height -1)
-    	return Math.max(leftH, rightH) + 1;
+        // check if node is balanced
+        if(Math.abs(leftSub - rightSub) > 1) balancedBool = false;
+        
+        return Math.max(leftSub, rightSub) +1;
+        
     }
 }
