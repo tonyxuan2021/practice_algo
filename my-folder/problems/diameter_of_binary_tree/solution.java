604d71f2a3d3a2cf3a7a4a7c4ea2b52376fd5996
tonyxuan2021 <tonyyexuan@gmail.com>
2022-09-19 16:39:44 +0000

    Sync LeetCode submission - Diameter of Binary Tree (java)

--- my-folder/problems/diameter_of_binary_tree/solution.java
+++ my-folder/problems/diameter_of_binary_tree/solution.java
@@ -1,31 +1,19 @@
-/* 
-Diameter of binary tree is length of longest path between any two nodes in a tree 
-and length is number of edges between them.
-Algo: Dive into leaves first and count the edges upwards (via recursion) because the longest length is between two leaf nodes.
-*/
-// T: O(n) - enter and exit from each node once
-// S: O(n) - depends on size of call stack which relates to height of the tree 
-    // worst case, the tree is skewed so height of the tree is O(n)
-    // if the tree is balanced, it will be O(log n)
 class Solution {
-    private int longestDiameter;
+    private int longestDiameter = 0;
     
     public int diameterOfBinaryTree(TreeNode root) {
-        longestDiameter = 0;
-        findLongestPath(root); // this updates longestDiameter
+        getDiameter(root);
         return longestDiameter;
     }
     
-    // recursive fx from leaf node up
-    private int findLongestPath(TreeNode root){
-        if(root == null) return 0; // null roots have path of 0
+    private int getDiameter(TreeNode node){
+        if(node == null) return 0;
         
-        int leftPath = findLongestPath(root.left);
-        int rightPath = findLongestPath(root.right);
+        int leftD = getDiameter(node.left);
+        int rightD = getDiameter(node.right);
         
-        longestDiameter = Math.max(longestDiameter, leftPath + rightPath);
+        longestDiameter = Math.max(longestDiameter, leftD + rightD);
         
-        return Math.max(leftPath, rightPath) + 1;
+        return Math.max(leftD, rightD) + 1;
     }
-
 }
