77484d07b9ae3e58127ba1a11ca1e4b48146da51
tonyxuan2021 <tonyyexuan@gmail.com>
2022-09-23 15:52:39 +0000

    Sync LeetCode submission - Lowest Common Ancestor of a Binary Tree (java)

--- my-folder/problems/lowest_common_ancestor_of_a_binary_tree/solution.java
+++ my-folder/problems/lowest_common_ancestor_of_a_binary_tree/solution.java
@@ -1,55 +1,93 @@
-// iterative using parent pointers
 class Solution {
+
+    // Three static flags to keep track of post-order traversal.
+
+    // Both left and right traversal pending for a node.
+    // Indicates the nodes children are yet to be traversed.
+    private static int BOTH_PENDING = 2;
+
+    // Left traversal done.
+    private static int LEFT_DONE = 1;
+
+    // Both left and right traversal done for a node.
+    // Indicates the node can be popped off the stack.
+    private static int BOTH_DONE = 0;
+
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        // stack for tree traversal
-        Deque<TreeNode> stack = new ArrayDeque<>();
-        // HashMap for parent pointers
-        Map<TreeNode, TreeNode> parentHM = new HashMap<>();
-        
-        parentHM.put(root, null);
-        stack.push(root);
-        
-        // iterate until we find both p and q nodes
-        while(!parentHM.containsKey(p) || !parentHM.containsKey(q)){
-            TreeNode node = stack.pop();
-            // save parent pointers as we traverse tree
-            if(node.left != null){
-                parentHM.put(node.left, node);
-                stack.push(node.left);
+
+        Stack<Pair<TreeNode, Integer>> stack = new Stack<Pair<TreeNode, Integer>>();
+
+        // Initialize the stack with the root node.
+        stack.push(new Pair<TreeNode, Integer>(root, Solution.BOTH_PENDING));
+
+        // This flag is set when either one of p or q is found.
+        boolean one_node_found = false;
+
+        // This is used to keep track of the LCA.
+        TreeNode LCA = null;
+
+        // Child node
+        TreeNode child_node = null;
+
+        // We do a post order traversal of the binary tree using stack
+        while (!stack.isEmpty()) {
+
+            Pair<TreeNode, Integer> top = stack.peek();
+            TreeNode parent_node = top.getKey();
+            int parent_state = top.getValue();
+
+            // If the parent_state is not equal to BOTH_DONE,
+            // this means the parent_node can't be popped off yet.
+            if (parent_state != Solution.BOTH_DONE) {
+
+                // If both child traversals are pending
+                if (parent_state == Solution.BOTH_PENDING) {
+
+                    // Check if the current parent_node is either p or q.
+                    if (parent_node == p || parent_node == q) {
+
+                        // If one_node_found was set already, this means we have found
+                        // both the nodes.
+                        if (one_node_found) {
+                            return LCA;
+                        } else {
+                            // Otherwise, set one_node_found to True,
+                            // to mark one of p and q is found.
+                            one_node_found = true;
+
+                            // Save the current top element of stack as the LCA.
+                            LCA = stack.peek().getKey();
+                        }
+                    }
+
+                    // If both pending, traverse the left child first
+                    child_node = parent_node.left;
+                } else {
+                    // traverse right child
+                    child_node = parent_node.right;
+                }
+
+                // Update the node state at the top of the stack
+                // Since we have visited one more child.
+                stack.pop();
+                stack.push(new Pair<TreeNode, Integer>(parent_node, parent_state - 1));
+
+                // Add the child node to the stack for traversal.
+                if (child_node != null) {
+                    stack.push(new Pair<TreeNode, Integer>(child_node, Solution.BOTH_PENDING));
+                }
+            } else {
+
+                // If the parent_state of the node is both done,
+                // the top node could be popped off the stack.
+                // Update the LCA node to be the next top node.
+                if (LCA == stack.pop().getKey() && one_node_found) {
+                    LCA = stack.peek().getKey();
+                }
+
             }
-            if(node.right != null){
-                parentHM.put(node.right, node);
-                stack.push(node.right);
-            }
         }
-        
-        // Ancestors set() for node p
-        Set<TreeNode> ancestors = new HashSet<>();
-        
-        // *** don't fully understand this part
-        // ancestors of p (5) which is 5 and 3... I think..
-        // p becomes null when we get ancestor of 3
-        // Process all ancestors for node p using parent pters
-        while(p != null){ //*** when will p be null?
-            ancestors.add(p);
-            p = parentHM.get(p);
-        }
-        
-        // First ancestor in q that appears in p's set() is LCA
-        while(!ancestors.contains(q)){
-            q = parentHM.get(q);
-        }
-        
-        return q;
+
+        return null;
     }
 }
-/*
-If we have parent pointers for each node we can traverse back from p and q. First common nose we get during traversal will be LCA node. We save parent pointers in a dictionary as we traverse the tree.
-
-Algo:
-1. start from root and traverse
-2. until we find p and q, store parent pointers in a dictionary
-3. once we found p and q, we get all ancestors for p using parent dictionary and add to a set called ancestors.
-4. similarly, we traverse through ancestors for q. If ancestor is present in ancestors set for p, this means this is the first ancestor common b/ween p and q (traversing upwards) and hence this is LCA node.
-*/
-
