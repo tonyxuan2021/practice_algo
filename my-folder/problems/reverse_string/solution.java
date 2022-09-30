056f49b4dc883038a5839f8e4722d1b28fc9281b
tonyxuan2021 <tonyyexuan@gmail.com>
2022-09-30 02:15:00 +0000

    Sync LeetCode submission - Reverse String (java)

--- my-folder/problems/reverse_string/solution.java
+++ my-folder/problems/reverse_string/solution.java
@@ -1,23 +1,27 @@
-/*
-e.g. s.length = 5
-input:  hello
-        01234
-output: olleh
-        01234
+/* 
+input:  h e l l o
+idx:    0 1 2 3 4
+
+output: o l l e h
+idx:    0 1 2 3 4
 */
+// T: O(n)
+// S: O(1)
 class Solution {
-    public void reverseString(char[] s){
-        helper(s, 0, s.length - 1);
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+        
+        helper(s, left, right);
     }
     
     private void helper(char[] s, int left, int right){
+        // base case
         if(left >= right) return;
         
-        // swap s[left] with s[right] and left++ and right--
-        char tmp = s[left];
-        
+        char temp = s[left];
         s[left++] = s[right];
-        s[right--] = tmp;
+        s[right--] = temp;
         
         helper(s, left, right);
     }
