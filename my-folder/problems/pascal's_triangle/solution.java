19f3cbf7c8192c4d16a5652059fc5433bc49d352
tonyxuan2021 <tonyyexuan@gmail.com>
2022-10-02 17:31:14 +0000

    Sync LeetCode submission - Pascal's Triangle (java)

--- my-folder/problems/pascal's_triangle/solution.java
+++ my-folder/problems/pascal's_triangle/solution.java
@@ -1,37 +1,33 @@
-// Dynamic Programming - recursion with memoization
-// 1 on left and right sides of each row
-// all other numbers are sum of 2 numbers above
-// easier if we visualize triangle left aligned
-// DP:
-// 1) optimal substructure 
-// 2) overlapping subproblems - some are used more than once
-// base case: value at beg and end is 1
-// recurrence: each # is sum of two #s above it
-// triangle[row][col] = triangle[row - 1][col - 1] + triangle[row - 1][col]
-// T: O(numRows ^ 2)
-// S: O(1) - input and output generally do not count towards space complexity
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> triangle = new ArrayList<>();
+        List<List<Integer>> pTriangle = new ArrayList<>();
+        // first row 
+        pTriangle.add(new ArrayList<Integer>());
+        // fill first row only elem with 1
+        pTriangle.get(0).add(1);
         
-        // add first row
-        triangle.add(new ArrayList<>());
-        // first row only has 1 single elem
-        triangle.get(0).add(1);
-        
+        // outer loop for rows
         for(int rowNum = 1; rowNum < numRows; rowNum++){
+            // create new ArrayList for each new row
             List<Integer> currRow = new ArrayList<>();
-            List<Integer> prevRow = triangle.get(rowNum - 1);
-            currRow.add(1); // base case
+            // get variable for prevRow
+            List<Integer> prevRow = pTriangle.get(rowNum - 1);
             
-            for(int col = 1; col < prevRow.size(); col++){
-                currRow.add(prevRow.get(col - 1) + prevRow.get(col));
+            // add 1 as first elem in currRow
+            currRow.add(1);
+            
+            // inner loop to fill the col of currRow (except first and last elem)
+            for(int colNum = 1; colNum < prevRow.size(); colNum++){
+                currRow.add(prevRow.get(colNum - 1) + prevRow.get(colNum));
             }
             
+            // add 1 as last elem in currRow
             currRow.add(1);
-            triangle.add(currRow);
+            // add currRow to pTriangle
+            pTriangle.add(currRow);
         }
         
-        return triangle;
+        // return our answer
+        return pTriangle;
     }
 }
