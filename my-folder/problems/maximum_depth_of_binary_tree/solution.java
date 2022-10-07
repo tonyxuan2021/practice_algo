b1f13ea1166f53bd520d8bd017bd565ce4c5138d
tonyxuan2021 <tonyyexuan@gmail.com>
2022-10-07 16:12:41 +0000

    Sync LeetCode submission - Maximum Depth of Binary Tree (java)

--- my-folder/problems/maximum_depth_of_binary_tree/solution.java
+++ my-folder/problems/maximum_depth_of_binary_tree/solution.java
@@ -1,32 +1,25 @@
-// Iterative solution - using two stacks to keep track of node and depth
-// T: O(n) - we have to visit every node
-// S: O(n) - worst case our stacks contains every node if binary tree is skewed
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public int maxDepth(TreeNode root) {
         if(root == null) return 0;
-        Deque<TreeNode> nodeStack = new LinkedList<>();
-        Deque<Integer> depthStack = new LinkedList<>();
         
-        nodeStack.push(root);
-        depthStack.push(1); // we begin at depth 1
+        int leftHeight = maxDepth(root.left);
+        int rightHeight = maxDepth(root.right);
         
-        int currDepth = 0, maxDepth = 0;
-        
-        while(!nodeStack.isEmpty()){
-            TreeNode currNode = nodeStack.pop();
-            currDepth = depthStack.pop();
-            
-            if(currNode != null){
-                maxDepth = Math.max(maxDepth, currDepth);
-                
-                nodeStack.push(currNode.left);
-                nodeStack.push(currNode.right);
-                
-                depthStack.push(currDepth + 1);
-                depthStack.push(currDepth + 1);
-            }
-        }
-        
-        return maxDepth;
+        return Math.max(leftHeight, rightHeight) + 1;
     }
 }
