de601d54d11d7551dd186fd87584d586940b822d
tonyxuan2021 <tonyyexuan@gmail.com>
2022-10-12 03:40:06 +0000

    Sync LeetCode submission - Same Tree (java)

--- my-folder/problems/same_tree/solution.java
+++ my-folder/problems/same_tree/solution.java
@@ -1,34 +1,44 @@
-// Iterative solution
-// T: O(n) - need to process every node
-// S: O(n) - last level (leaf nodes) there are n/2 or n in our queue
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
+    // BFS
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null && q == null) return true;
-        if(!check(p, q)) return false;
+        Queue<TreeNode> pQueue = new LinkedList<>();
+        Queue<TreeNode> qQueue = new LinkedList<>();
         
-        // queues
-        Queue<TreeNode> queueP = new ArrayDeque<>();
-        Queue<TreeNode> queueQ = new ArrayDeque<>();
-        queueP.add(p);
-        queueQ.add(q);
+        pQueue.add(p);
+        qQueue.add(q);
         
-        while(!queueP.isEmpty()){
-            p = queueP.poll();
-            q = queueQ.poll();
+        while(!pQueue.isEmpty()){
+            p = pQueue.poll();
+            q = qQueue.poll();
             
             if(!check(p, q)) return false;
             
             if(p != null){
                 if(!check(p.left, q.left)) return false;
                 if(p.left != null){
-                    queueP.add(p.left);
-                    queueQ.add(q.left);
-                }
+                    pQueue.add(p.left);
+                    qQueue.add(q.left);
+                } 
                 
                 if(!check(p.right, q.right)) return false;
                 if(p.right != null){
-                    queueP.add(p.right);
-                    queueQ.add(q.right);
+                    pQueue.add(p.right);
+                    qQueue.add(q.right);
                 }
             }
         }
@@ -37,7 +47,9 @@ class Solution {
     }
     
     private boolean check(TreeNode p, TreeNode q){
+        // guard against nulls
         if(p == null && q == null) return true;
+        // if only p or q is null
         if(p == null || q == null) return false;
         if(p.val != q.val) return false;
         return true;
